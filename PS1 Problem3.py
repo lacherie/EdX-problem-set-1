16c394916404495d524ec6f3c96fb8529b55bd4f
lacherie <55232600+lacherie@users.noreply.github.com>
2019-09-23 09:59:13 +0200

    Update PS1 Problem3.py

--- PS1 Problem3.py
+++ PS1 Problem3.py
@@ -1,32 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 21 09:56:43 2019
-
-@author: admin
-"""
-
-
-s = 'abzcbobobegghakl'
-str_input = s
-#s = 'abcdefg'
-substr = ''
-#x = 0
-#y = x + 1
-#for x in range(0,len(s)):
-#    while s[x] <= s[x + 1]:
-#        substr = s[x : y]
-#        y += 1
-#        print(s[x])
-#
-#for x in range(0,len(s)):
-#    y = x
-#    while s[y] <= s[y + 1]:
-#        substr = s[start_substr : y + 1]
-#        y += 1
-#    else:
-#        substr = ''
-#        start_substr = y
-        
 start = 0
 x = 0
 longest_substr = ''
